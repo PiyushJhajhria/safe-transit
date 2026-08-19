d97c085d6f8b2d7738ba6815ac95d7c84e7061e1
PiyushJhajhria <piyush2007w@gmail.com>
2026-08-19 17:19:30 +0530

    added project planning documents

---
+++ frontend/app/control/page.tsx
@@ -0,0 +1,7 @@
+export default function Control() {
+  return (
+    <div>
+      <h1>Control Dashboard</h1>
+    </div>
+  );
+}
---
+++ frontend/app/volunteer/page.tsx
@@ -0,0 +1,7 @@
+export default function Volunteer() {
+  return (
+    <div>
+      <h1>Volunteer Dashboard</h1>
+    </div>
+  );
+}
